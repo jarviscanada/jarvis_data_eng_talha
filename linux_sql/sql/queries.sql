d01df8eeb5a82898f2f620ceb236c17d4e8d4055
talha <talha7171@gmail.com>
2019-11-30 01:31:48 +0000

    Finished second query

--- linux_sql/sql/queries.sql
+++ linux_sql/sql/queries.sql
@@ -16,3 +16,20 @@ order by
 -- next is to get Average memory usage
 --Average used memory in percentage over 5 mins interval for each host. (used memory = total memory - free memory).
 
+select 
+	host_usage.host_id as host_id, 
+	host_info.host_name as host_name, 
+	host_info.total_mem as total_memory, 
+	avg(
+		((host_info.total_mem - host.usage.memory_free) / host_info.total_mem) * 100) 
+		as used_memory_percentage 
+from 	
+	host_usage 
+	inner join host_info on host_usage.host_id=host_info.id 
+group by
+	host_id, 
+	host_name, 
+	total_memory, 
+	DATE_TRUNC('hour', host_usage.timestamp) + DATE_PART('minute', host_usage.timestamp)::int / 5 * interval '5 min' as time 
+order by 
+	host_usage.host_id;
