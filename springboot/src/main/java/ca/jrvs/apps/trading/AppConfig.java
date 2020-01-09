44421a2ceee9f175a796daba9c0d0e531326e798
talha <talha7171@gmail.com>
2020-01-09 21:11:34 +0000

    Completed showIexQuote ticket

--- springboot/src/main/java/ca/jrvs/apps/trading/AppConfig.java
+++ springboot/src/main/java/ca/jrvs/apps/trading/AppConfig.java
@@ -1,4 +1,33 @@
 package ca.jrvs.apps.trading;
 
+import ca.jrvs.apps.trading.dao.MarketDataDao;
+import ca.jrvs.apps.trading.model.config.MarketDataConfig;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Component
+//@EnableTransactionManagement
 public class AppConfig {
+    private Logger logger = LoggerFactory.getLogger(AppConfig.class);
+
+    @Bean
+    public MarketDataConfig marketDataConfig() {
+        MarketDataConfig marketDataConfig = new MarketDataConfig();
+        marketDataConfig.setHost("https://cloud.iexapis.com/v1/");
+        marketDataConfig.setToken(System.getenv("IEX_PUB_TOKEN"));
+
+        return marketDataConfig;
+    }
+
+    @Bean
+    public PoolingHttpClientConnectionManager poolingHttpClientConnectionManager() {
+        PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager();
+        cm.setMaxTotal(50);
+        cm.setDefaultMaxPerRoute(50);
+        return cm;
+    }
 }
--- springboot/src/main/java/ca/jrvs/apps/trading/controller/QuoteController.java
+++ springboot/src/main/java/ca/jrvs/apps/trading/controller/QuoteController.java
@@ -1,4 +1,34 @@
 package ca.jrvs.apps.trading.controller;
 
+import ca.jrvs.apps.trading.model.domain.IexQuote;
+import ca.jrvs.apps.trading.service.QuoteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+//@Api(value = "quote", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+@Controller
+@RequestMapping("/quote")
 public class QuoteController {
+
+    private QuoteService quoteService;
+
+    @Autowired
+    public QuoteController(QuoteService quoteService) {
+        this.quoteService = quoteService;
+    }
+
+    @GetMapping(path = "/iex/ticker/{ticker}")
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public IexQuote getQuote(@PathVariable String ticker) {
+        try {
+            return quoteService.findIexQuoteByTicker(ticker);
+        }
+        catch (Exception e) {
+            throw ResponseExceptionUtil.getResponseStatusException(e);
+        }
+    }
+
 }
