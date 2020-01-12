92e0453c0c5e6f4114078128e659dcc8d5a6f0e1
talha <talha7171@gmail.com>
2020-01-12 06:59:33 +0000

    Updated Quote Controller

--- springboot/src/main/java/ca/jrvs/apps/trading/controller/QuoteController.java
+++ springboot/src/main/java/ca/jrvs/apps/trading/controller/QuoteController.java
@@ -1,11 +1,14 @@
 package ca.jrvs.apps.trading.controller;
 
 import ca.jrvs.apps.trading.model.domain.IexQuote;
+import ca.jrvs.apps.trading.model.domain.Quote;
 import ca.jrvs.apps.trading.service.QuoteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 //@Api(value = "quote", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 @Controller
@@ -36,6 +39,42 @@ public class QuoteController {
     public void updateMarketData() {
         try {
             quoteService.updateMarketData();
+        }
+        catch (Exception e) {
+            throw ResponseExceptionUtil.getResponseStatusException(e);
+        }
+    }
+
+    @PutMapping(path = "/")
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public Quote putQuote(@RequestBody Quote quote) {
+        try {
+            return quoteService.saveQuote(quote);
+        }
+        catch (Exception e) {
+            throw ResponseExceptionUtil.getResponseStatusException(e);
+        }
+    }
+
+    @PostMapping(path = "/tickerId/{tickerId}")
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public Quote createQuote(@PathVariable String tickerId) {
+        try {
+            return quoteService.saveQuote(tickerId);
+        }
+        catch (Exception e) {
+            throw ResponseExceptionUtil.getResponseStatusException(e);
+        }
+    }
+
+    @GetMapping(path = "/dailyList")
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public List<Quote> getDailyList() {
+        try {
+            return quoteService.findAllQuotes();
         }
         catch (Exception e) {
             throw ResponseExceptionUtil.getResponseStatusException(e);
