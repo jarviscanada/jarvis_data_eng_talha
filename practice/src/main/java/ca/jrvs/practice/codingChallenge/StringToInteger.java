b678f5ed9becfbcf6ca5c83e45fd4f198a741102
Talha <talha7171@gmail.com>
2020-02-11 11:44:32 -0500

    Updated String to integer ticket

--- practice/src/main/java/ca/jrvs/practice/codingChallenge/StringToInteger.java
+++ practice/src/main/java/ca/jrvs/practice/codingChallenge/StringToInteger.java
@@ -16,10 +16,13 @@ public class StringToInteger {
         if (input.isEmpty()) {
             return 0;
         }
-        int sign = 1; int i = 0;
+        int sign = 1;
+        int i = 0;
 
         if (input.charAt(0) == '-' || input.charAt(0) == '+') {
-            sign = (input.charAt(0) == '-') ? -1 : 1;
+            if (input.charAt(0) == '-') {
+                sign = -1;
+            }
             if (input.length() < 2 || !Character.isDigit(input.charAt(1))) {
                 return 0;
             }
@@ -32,7 +35,12 @@ public class StringToInteger {
                 int d = input.charAt(i) - '0';
                 if (n > (Integer.MAX_VALUE - d) / 10) {
                     //Check for integer overflow
-                    n = (sign == -1) ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+                    if (sign == -1) {
+                        n = Integer.MIN_VALUE;
+                    }
+                    else {
+                        n = Integer.MAX_VALUE;
+                    }
                     return n;
                 }
                 n = n*10 + d;
